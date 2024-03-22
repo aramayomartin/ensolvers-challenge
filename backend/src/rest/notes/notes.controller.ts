d52b6fe504c34d617bad63077918011597596ac2
aramayomartin <raul.aramayo@ib.edu.ar>
2024-03-22 18:09:31 -0300

    Add readme

--- backend/src/rest/notes/notes.controller.ts
+++ backend/src/rest/notes/notes.controller.ts
@@ -36,11 +36,6 @@ export class NotesController {
     return this.notesService.findAllByUser(+userId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
-  }
-
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
     return this.notesService.update(+id, updateNoteDto);
